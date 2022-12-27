8bbbf071162193050658b3fc0a2c4da8b1c05e52
saif8983 <100688655+saif8983@users.noreply.github.com>
2022-12-27 14:34:38 +0530

    main page

--- src/Components/chartDetail.js
+++ src/Components/chartDetail.js
@@ -1,17 +1,49 @@
 import { Dimensions,View,Text ,StyleSheet} from "react-native";
-import { NativeBaseProvider ,Select,VStack,HStack,Button,Input} from "native-base";
-import React, { useState } from 'react';
+import { NativeBaseProvider ,Modal,Select,VStack,HStack,Button,Input} from "native-base";
+import React, { useState ,useMemo,useEffect} from 'react';
 import CalendarPicker from 'react-native-calendar-picker';
 import { LineChart, } from "react-native-chart-kit";
+import currencyApi from "../api/currencyApi";
 const screenWidth = Dimensions.get("window").width;
 
-const ChartDetail=()=>{
+const ChartDetail=({serviceFirst,serviceSecond})=>{
   const [startDate, setStartDate] = useState(null);
-const string=startDate===null?null:startDate.join('')
-console.log(string)
+const [endDate,setEndDate]=useState(null)
+  const [modalStartDate, setModalStartDate] = useState(false);
+  const [modalEndDate,setModalEndDate]=useState(false)
+  const [lablsOfChart,setLableOfChart]=useState(['jan','feb','mar','apr'])
+  const [dataOfChart,setDataOfChart]=useState(['0','1','3','4'])
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const startOfPreviousWeek = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate() - selectedDate.getDay() - 6);
+  const start_date = new Date(startDate);
+const start_string = start_date.toISOString().split('T')[0];
+const end_date=new Date(endDate)
+const end_string=end_date.toISOString().split('T')[0];
+const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
+const timeSeries=async()=>{
+  const response= await currencyApi.get('/timeseries',{
+    params:{
+      base:serviceFirst,
+      start_date:start_string,
+      end_date:end_string
+    }
+  })
+  const data=response.data.rates
+  const dateArray=Object.keys(data)
+  const dayNameForDates=dateArray.map(date => {
+    const day = new Date(date).getDay();
+    return dayNames[day];
+  });
+  setLableOfChart(dayNameForDates)
+  const inrValues = Object.values(data).map(rates => rates.INR);
+setDataOfChart(inrValues)
+}
+useEffect(()=>{
+ timeSeries()
+},[startDate,endDate])
   const chartConfig = {
     backgroundGradientFrom: "#1E2923",
-    backgroundGradientFromOpacity: 0,
+    backgroundGradientFromOpacity: 1,
     backgroundGradientTo: "#08130D",
     backgroundGradientToOpacity: 0.5,
     color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
@@ -20,24 +52,37 @@ console.log(string)
     useShadowColorFromDataset: false // optional
   };
 const data = {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: lablsOfChart,
     datasets: [
       {
-        data: [20, 45, 28, 80, 99, 43],
+        data: dataOfChart,
         color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
         strokeWidth: 2 // optional
       }
     ],
-    legend: ["Rainy Days"] // optional
+    legend: [`${serviceFirst}/${serviceSecond}`] // optional
   };
   const handleStartDateChange = (newDate) => {
     setStartDate(newDate);
+    setModalStartDate(false)
   };
 
   const handleEndDateChange = (newDate) => {
     setEndDate(newDate);
+    setModalEndDate(false)
   };
-
+  const handleOpenCalenderStartDate = useMemo(() => {
+    return () => {
+      setModalStartDate(true);
+      
+    };
+  }, [modalStartDate]);
+  
+  const handleOpenCalenderEndDate = useMemo(() => {
+    return () => {
+      setModalEndDate(true);
+    };
+  }, [modalEndDate]);
 return(
   <NativeBaseProvider>
   <VStack alignItems="center" space={4}>
@@ -47,23 +92,35 @@ return(
   height={220}
   chartConfig={chartConfig}
 />
+<HStack space={6} justifyContent="center" >
+        <Button size="xs" style={styles.button} onPress={()=>handleOpenCalenderStartDate()}>Start date</Button>
+        <Button size="xs" style={styles.button} onPress={()=>handleOpenCalenderEndDate()}>End date</Button>
+      </HStack>
 <HStack space={3} justifyContent="center" style={{height:300}}>
+<Modal style={styles.modal} isOpen={modalStartDate} >
 <CalendarPicker
+
         selected={startDate}
-        onDateChange={handleStartDateChange}
-        
+        onDateChange={(newDate)=>handleStartDateChange(newDate)}
         todayBackgroundColor="#f2e6ff"
           selectedDayColor="#7300e6"
           selectedDayTextColor="#FFFFFF"
+          minDate={startOfPreviousWeek}
+        maxDate={selectedDate}
       />
-      {/* <CalendarPicker
+</Modal>
+<Modal  isOpen={modalEndDate} style={styles.modal}>
+      <CalendarPicker
         selected={endDate}
-        onChange={handleEndDateChange}
+        onDateChange={(newDate)=>handleEndDateChange(newDate)}
         todayBackgroundColor="#f2e6ff"
           selectedDayColor="#7300e6"
           selectedDayTextColor="#FFFFFF"
-          
-      /> */}
+          minDate={startOfPreviousWeek}
+        maxDate={selectedDate}
+        
+      />
+      </Modal>
 </HStack>
 </VStack>
 </NativeBaseProvider>
@@ -71,6 +128,13 @@ return(
 )
 }
 const styles=StyleSheet.create({
-
+  modal:{
+    backgroundColor:'grey',
+  maxHeight:300,
+  marginTop:300
+},
+button:{
+  borderRadius:20
+}
 })
 export default ChartDetail;
--- src/screens/componentScreen.js
+++ src/screens/componentScreen.js
@@ -1,12 +1,26 @@
 import React ,{useEffect}from "react";
-import { NativeBaseProvider ,Select,VStack,HStack,Button,Text,Input,ScrollView} from "native-base";
+import { NativeBaseProvider ,Select,VStack,HStack,Button,Text,Input,ScrollView,Alert,Center,IconButton,CloseIcon,Box} from "native-base";
 import ChartDetail from "../Components/chartDetail";
+import { useNetInfo } from "@react-native-community/netinfo";
 import currencyApi from "../api/currencyApi";
 const componentScreen=()=>{
+  
+  const netInfo=useNetInfo()
+  netInfo.isConnected
+  
+  console.log(netInfo.isConnected)
+  useEffect(() => {
+    if (netInfo.isConnected) {
+      setAlert(false)
+    }else{
+      setAlert(true)
+    }
+  }, [netInfo.isConnected]);
     const [serviceFirst, setServiceFirst] = React.useState("");
     const [serviceSecond, setServiceSecond] = React.useState("");
     const [inputAmountValue,setInputAmountValue]=React.useState(0)
     const [amount,setAmount]=React.useState(0)
+    const [alert,setAlert]=React.useState(false)
 const [currencyFirst,setCurrencyFirst]=React.useState([])
     const currency_name=async()=>{
       try{
@@ -40,29 +54,55 @@ console.log(error)
       }
     }
     return (
-        <NativeBaseProvider>
-      <ScrollView>
-      <VStack alignItems="center" space={4}>
 
-      <Select shadow={2} selectedValue={serviceFirst} minWidth="200" accessibilityLabel="Choose Service" placeholder="Choose Service"  onValueChange={itemValue => setServiceFirst(itemValue)}>
+        <NativeBaseProvider>
+        {alert ? (<Center>
+      <Alert maxW="400" status="info" colorScheme="info">
+        <VStack space={2} flexShrink={1} w="100%">
+          <HStack flexShrink={1} space={2} alignItems="center" justifyContent="space-between">
+            <HStack flexShrink={1} space={2} alignItems="center">
+              <Alert.Icon />
+              <Text fontSize="md" fontWeight="medium" color="coolGray.800">
+                Internet is not connected
+              </Text>
+            </HStack>
+            <IconButton variant="unstyled" _focus={{
+            borderWidth: 0
+          }}  />
+          </HStack>
+          <Box pl="6" _text={{
+          color: "coolGray.600"
+        }}>
+            Please check your connection
+          </Box>
+        </VStack>
+      </Alert>
+    </Center>):
+      <ScrollView style={{backgroundColor:'black'}}>
+      <Button onPress={() => console.log("hello world")} variant="ghost" >+</Button>
+      <VStack alignItems="center" space={4}>
+<Text>From</Text>
+      <Select shadow={2} color="white" selectedValue={serviceFirst} minWidth="200" accessibilityLabel="Choose Service" placeholder="Choose Service"  onValueChange={itemValue => setServiceFirst(itemValue)}>
         {currencyFirst.map((item,index)=>{
             return <Select.Item key={index} shadow={2} label={item} value={item} />
         })}
       </Select>
-      <Select shadow={2} selectedValue={serviceSecond} minWidth="200" accessibilityLabel="Choose Service" placeholder="Choose Service"  onValueChange={itemValue => setServiceSecond(itemValue)}>
+      <Text>To</Text>
+      <Select shadow={2} color="white" selectedValue={serviceSecond} minWidth="200" accessibilityLabel="Choose Service" placeholder="Choose Service"  onValueChange={itemValue => setServiceSecond(itemValue)}>
         {currencyFirst.map((item,index)=>{
             return <Select.Item key={index} shadow={2} label={item} value={item} />
         })}
       </Select>
-      <Input size="md" minWidth="200" placeholder="Amount" type="number" value={inputAmountValue} onChangeText={(Input)=>handleChangeValue(Input)} />
+      <Text>Amount</Text>
+      <Input size="md" color="white" minWidth="200" placeholder="Amount" type="number" value={inputAmountValue} onChangeText={(Input)=>handleChangeValue(Input)} />
       <HStack space={3} justifyContent="center">
-      <Button onPress={() => console.log("hello world")}>+Add Watchlist</Button>
-      <Button onPress={() => convertCurrency()}>Convert</Button>
+      <Button onPress={() => convertCurrency()}  >Convert</Button>
       </HStack>
       <Text bold>Amount:{amount}</Text>
-      <ChartDetail/>
+      <ChartDetail serviceFirst={serviceFirst} serviceSecond={serviceSecond}/>
       </VStack>
       </ScrollView>
+        }
     </NativeBaseProvider>
     
     )
